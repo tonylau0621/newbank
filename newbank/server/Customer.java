0da060fb0aff994cb0b54918758b6f034ba25831
Jarupath Kulsiriruangyos <jk2061@bath.ac.th>
2022-03-15 01:02:39 +0700

    update code from feature/customer-related-structure branch into log-in branch

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -3,13 +3,23 @@ package newbank.server;
 import java.util.ArrayList;
 
 public class Customer {
-	
+
+	private CustomerID customerID;
 	private ArrayList<Account> accounts;
-	
-	public Customer() {
-		accounts = new ArrayList<>();
+
+	public Customer(CustomerID customerID, ArrayList<Account> accounts) {
+		this.customerID = customerID;
+		this.accounts = accounts;
 	}
-	
+
+	public Customer(CustomerID customerID) {
+		this(customerID, new ArrayList<>());
+	}
+
+	public CustomerID getCustomerID() {
+		return customerID;
+	}
+
 	public String accountsToString() {
 		String s = "";
 		for(Account a : accounts) {
@@ -19,6 +29,6 @@ public class Customer {
 	}
 
 	public void addAccount(Account account) {
-		accounts.add(account);		
+		accounts.add(account);
 	}
 }
--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -4,35 +4,41 @@ import java.util.HashMap;
 
 public class NewBank {
 
-	//Testing
+
 	private static final NewBank bank = new NewBank();
 	private HashMap<String,Customer> customers;
-	
+
 	private NewBank() {
 		customers = new HashMap<>();
 		addTestData();
 	}
-	
+
 	private void addTestData() {
-		Customer bhagy = new Customer();
+		CustomerID bhagyID = new CustomerID("Bhagy", "bhagyPass", "Bhagy", "Brown", "07654321987", "bhagyishappy@gmail.com", "123 Wonder Street, London AB1 2YZ");
+		Customer bhagy = new Customer(bhagyID);
 		bhagy.addAccount(new Account("Main", 1000.0));
-		customers.put("Bhagy", bhagy);
-		
-		Customer christina = new Customer();
+		customers.put(bhagy.getCustomerID().getKey(), bhagy);
+
+
+		CustomerID christinaID = new CustomerID("Christina", "christinaPass");
+		Customer christina = new Customer(christinaID);
 		christina.addAccount(new Account("Savings", 1500.0));
-		customers.put("Christina", christina);
-		
-		Customer john = new Customer();
+		customers.put(christina.getCustomerID().getKey(), christina);
+
+		CustomerID johnID = new CustomerID("John", "johnPass");
+		Customer john = new Customer(johnID);
 		john.addAccount(new Account("Checking", 250.0));
-		customers.put("John", john);
+		customers.put(john.getCustomerID().getKey(), john);
 	}
-	
+
 	public static NewBank getBank() {
 		return bank;
 	}
-	
+
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
-		if(customers.containsKey(userName)) {
+		// some dummy code demonstrating how to check password.
+		Customer customer = customers.get(userName);
+		if(customer != null && customer.getCustomerID().checkPassword(password)) {
 			return new CustomerID(userName);
 		}
 		return null;
@@ -42,15 +48,14 @@ public class NewBank {
 	public synchronized String processRequest(CustomerID customer, String request) {
 		if(customers.containsKey(customer.getKey())) {
 			switch(request) {
-			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
-			default : return "FAIL";
+				case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
+				default : return "FAIL";
 			}
 		}
 		return "FAIL";
 	}
-	
+
 	private String showMyAccounts(CustomerID customer) {
 		return (customers.get(customer.getKey())).accountsToString();
 	}
-
 }
