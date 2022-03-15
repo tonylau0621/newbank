92e584cd58642771839a0ad795f68cf097fc7f56
mkoschinski <mkk50@bath.ac.uk>
2022-03-15 22:47:57 +0100

    Fixed payAmount and solved negative value issue

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -1,73 +1,187 @@
 package newbank.server;
 
+// Only user for testing
+//import java.util.ArrayList;
 import java.util.HashMap;
 
+// Please comment out all "Only use for testing" method/block/statement for real use.
 public class NewBank {
 
-	//Testing
 	private static final NewBank bank = new NewBank();
 	private HashMap<String,Customer> customers;
-	
+
+	// Only use for testing
+	//public ArrayList<CustomerID> customersID;
+
 	private NewBank() {
 		customers = new HashMap<>();
 		addTestData();
 	}
-	
+
 	private void addTestData() {
-		Customer bhagy = new Customer();
+		CustomerID bhagyID = new CustomerID("Bhagy", "bhagyPass", "Bhagy", "Brown", "07654321987", "bhagyishappy@gmail.com", "123 Wonder Street, London AB1 2YZ");
+		Customer bhagy = new Customer(bhagyID);
 		bhagy.addAccount(new Account("Main", 1000.0));
-		customers.put("Bhagy", bhagy);
-		
-		Customer christina = new Customer();
+		customers.put(bhagy.getCustomerID().getKey(), bhagy);
+
+
+		CustomerID christinaID = new CustomerID("Christina", "christinaPass");
+		Customer christina = new Customer(christinaID);
 		christina.addAccount(new Account("Savings", 1500.0));
-		customers.put("Christina", christina);
-		
-		Customer john = new Customer();
+		customers.put(christina.getCustomerID().getKey(), christina);
+
+		CustomerID johnID = new CustomerID("John", "johnPass");
+		Customer john = new Customer(johnID);
 		john.addAccount(new Account("Checking", 250.0));
-		customers.put("John", john);
+		customers.put(john.getCustomerID().getKey(), john);
 	}
-	
+
 	public static NewBank getBank() {
 		return bank;
 	}
-	
-	public synchronized CustomerID checkLogInDetails(String userName, String password) {
-		if(customers.containsKey(userName)) {
-			return new CustomerID(userName);
-		}
-		return null;
+
+	public synchronized CustomerID checkLogInDetails(String userName, String password) throws InvalidUserNameException, InvalidPasswordException {
+		boolean isValidUserName = (customers.containsKey(userName));
+		if(!isValidUserName) throw new InvalidUserNameException();
+		Customer targetCustomer = customers.get(userName);
+		CustomerID targetCustomerId = targetCustomer.getCustomerID();
+		boolean isValidPassword = (targetCustomerId.checkPassword(password));
+		if(!isValidPassword) throw new InvalidPasswordException();
+		return targetCustomerId;
 	}
 
 	// commands from the NewBank customer are processed in this method
 	public synchronized String processRequest(CustomerID customer, String request) {
+		String[] requestTokens = request.split("\\s+");
+
 		if(customers.containsKey(customer.getKey())) {
-			switch(request) {
-			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
-			default : return "FAIL";
+			switch(requestTokens[0]) {
+				case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
+				case "NEWACCOUNT" :
+					if (requestTokens.length > 1) {
+						return newAccount(customer, requestTokens[1]);
+					}
+				case "MOVE" :
+					if (requestTokens.length > 3) {
+						try {
+							return moveAmount(Double.parseDouble(requestTokens[1]), requestTokens[2], requestTokens[3], customer);
+						} catch (NumberFormatException e) {
+							return "FAIL";
+						}
+					}
+				case "PAY" :
+					if (requestTokens.length > 4) {
+						try {
+							return payAmount(Double.parseDouble(requestTokens[1]), customer, requestTokens[2],
+									(requestTokens[3]),requestTokens[4]);}
+						catch (Exception e) {
+							return "FAIL";
+						}
+					}
+				default : return "FAIL";
 			}
 		}
 		return "FAIL";
 	}
-	
+
 	private String showMyAccounts(CustomerID customer) {
 		return (customers.get(customer.getKey())).accountsToString();
 	}
 
+	private String newAccount(CustomerID customer, String accountName) {
+		if(customers.keySet().contains(customer.getKey())){
+			customers.get(customer.getKey()).addAccount(new Account(accountName, 0.0));
+			return "SUCCESS";
+		}
+		return "FAIL";
+	}
+
+	private String moveAmount(double amount, String from, String to, CustomerID customer){
+		//Account does not exist
+		Customer target = customers.get(customer.getKey());
+		if (target.getAccount(from) == null || target.getAccount(to) == null){
+			//return "Account does not exist";
+			return "FAIL";
+		}
+		//Not enough balance
+		if (target.getAccount(from).getAmount() < amount){
+			//return "Not enough amount in your account";
+			return "FAIL";
+		}
+		//Update the amount and return success
+		target.getAccount(from).updateBalance(-amount);
+		target.getAccount(to).updateBalance(amount);
+		return "SUCCESS";
+	}
+
+	// PAY command
+	//Do I want to use Accounts or Customers?
 	// payAmount: Takes two accounts and an amount as input
-	public static void payAmount(Customer receivingAccount, Customer payingAccount, int Amount) {
-		// getBalance checks the current balance for the payingAccount Customer
-		int balance = receivingAccount.getBalance();
-		if(balance > Amount) {
-			// setBalance changes the current balance for the payingAccount Customer
-			receivingAccount.setBalance((balance-Amount));
-			new_balance = receivingAccount.getBalance()
-			System.out.println("Payment was successful. New Balance:");
-			System.out.println(new_balance);
+	private String payAmount(double amount, CustomerID payingCustomer, String payingAccount,
+							 String receivingCustomerKey, String receivingAccount ) {
+		//Account does not exist
+		Customer payer = customers.get(payingCustomer.getKey());
+		Customer receiver = customers.get(receivingCustomerKey);
+
+		if (payer.getAccount(payingAccount) == null || receiver.getAccount(receivingAccount) == null) {
+			//return "Please check Accounts";
+			return "FAIL";
+		}
+		//Customers should not be able to transfer less than 0.01
+		if(amount < 0.01) {return "FAIl";}
+
+		//get Account Balance Amount
+		double balance = payer.getAccount(payingAccount).getAmount();
+		//Is there enough balance?
+		if(balance > amount) {
+			//Adjust balance for both accounts
+			payer.getAccount(payingAccount).updateBalance(-amount);
+			receiver.getAccount(receivingAccount).updateBalance(amount);
+
+			//updated_balance = payer.getAccount(payingAccount).getAmount();
+
+			//return "Payment was successful.";
+			return "SUCCESS";
 		}
 		else {
-			System.out.println("Insufficient balance.");
+			//return "Insufficient balance.";
+			return "FAIL";
 		}
 
 	}
 
+	/*
+	// Only use for testing
+	public void resetTestData() {
+		customers = new HashMap<>();
+		CustomerID bhagyID = new CustomerID("Bhagy", "bhagyPass", "Bhagy", "Brown", "07654321987", "bhagyishappy@gmail.com", "123 Wonder Street, London AB1 2YZ");
+		Customer bhagy = new Customer(bhagyID);
+		bhagy.addAccount(new Account("Main", 1000.0));
+		customers.put(bhagy.getCustomerID().getKey(), bhagy);
+
+
+		CustomerID christinaID = new CustomerID("Christina", "christinaPass");
+		Customer christina = new Customer(christinaID);
+		christina.addAccount(new Account("Savings", 1500.0));
+		customers.put(christina.getCustomerID().getKey(), christina);
+
+		CustomerID johnID = new CustomerID("John", "johnPass");
+		Customer john = new Customer(johnID);
+		john.addAccount(new Account("Checking", 250.0));
+		customers.put(john.getCustomerID().getKey(), john);
+
+		customersID = new ArrayList<>();
+		customersID.add(bhagyID);
+		customersID.add(christinaID);
+		customersID.add(johnID);
+	}
+
+	// Only use for testing
+	public HashMap<String,Customer> getCustomers() {
+		return customers;
+	}
+	*/
+
 }
+
+
